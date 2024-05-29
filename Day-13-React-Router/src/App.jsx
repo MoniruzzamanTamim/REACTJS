a66184a571b2730476bf83caeac9c0cb6b7afdad
Tamim <tamimiqbal896@gmail.com>
2024-05-29 22:04:00 +0600

    F

--- Day-13-React-Router/src/App.jsx
+++ Day-13-React-Router/src/App.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+import ErrorPage from './Pages/ErrorPage/ErrorPage';
+import Header from './Pages/Header/Header';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+        <Route path='/' element ={<Home/>} />
+        <Route path='/home' element ={<Home/>} />
+        <Route path='/about' element ={<About/>} />
+        <Route path='/contact' element ={<Contact/>} />
+        <Route path='/*' element ={<ErrorPage/>} />
+      
+    </Routes>
+    </BrowserRouter>
+
     </>
   )
 }
---
+++ Day-13-React-Router/src/Pages/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div>
+        <ul>
+          <li><NavLink to={"/"}>Home</NavLink></li>
+          <li><NavLink to={"/about"}>About</NavLink></li>
+          <li><NavLink to={"/contact"}>Contact</NavLink></li>
+          <li></li>
+          <li></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Header
